e4f207c79941c336403c4cabfaa27dfab85b27d1
Peter Piliar <mr.p.piliar@gmail.com>
2019-08-14 19:01:11 +0200

    Change build to portable

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,12 @@ module.exports = {
       electronBuilder: {
         builderOptions: {
           // options placed here will be merged with default configuration and passed to electron-builder
+          win: {
+            target: ["portable"]
+          },
+          portable: {
+            artifactName: "${productName}_Portable_${version}.exe"
+          },
           nsis: {
             deleteAppDataOnUninstall: true
           }
