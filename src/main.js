fc54f38679af06779d927b6c1208e0fa3f645bd3
Peter Piliar <mr.p.piliar@gmail.com>
2019-09-28 19:24:01 +0200

    Change locale to sk

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale/lang/sk';
 // import './assets/element-variables.scss'
 
 Vue.use(ElementUI, {locale});
