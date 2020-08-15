d669d12715e3933be1b06fdfe005d1612d72326c
Peter Piliar <mr.p.piliar@gmail.com>
2020-08-15 13:24:59 +0200

    Update deps and filesUtil

--- src/background.js
+++ src/background.js
@@ -28,8 +28,13 @@ function createWindow () {
   win = new BrowserWindow({ width: 900, height: 600, 
     title: 'Rekreačné poukazy',
     webPreferences: {
-    nodeIntegration: true,
-    devTools: false //disable devTools in production build
+    // Use pluginOptions.nodeIntegration, leave this alone 
+    // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info 
+    nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+    devTools: isDevelopment //disable devTools in production build
+    //secure ??
+    //contextIsolation: true,
+    //sandbox: true
   } })
 
   win.on('page-title-updated', (evt) => {
--- src/scripts/fileUtil.js
+++ src/scripts/fileUtil.js
@@ -1,5 +1,8 @@
+import { createWriteStream } from 'fs';
+
 const path = require('path');
 const fs = require('fs');
+//const app = require('electron').remote.app;
 
 function ensureExists(path, mask, cb) {
     if (typeof mask == 'function') { // allow the `mask` parameter to be optional
@@ -14,9 +17,54 @@ function ensureExists(path, mask, cb) {
     });
 }
 
-export default function checkDataFiles(userDataPath){
+export function checkDataFiles(userDataPath){
     var dataFolder = path.join(userDataPath,"data");
     ensureExists(dataFolder, (err) => {
         if (err) throw err;
     });
+}
+
+export function createPortableAppDataFolder(portableAppDataPath) {
+    var dataFolder = path.join(portableAppDataPath);
+    ensureExists(dataFolder, (err) => {
+        if (err) throw err;
+    });
+}
+
+export function copyFromRoaming(userDataPath, portableAppDataPath) {
+    let dbPath = path.join(userDataPath, "data", "rekredb.db"); 
+    let newPath = path.join(portableAppDataPath, "rekredb.db");
+    
+    if (!fs.existsSync(newPath) && fs.existsSync(dbPath)) {
+        fs.copyFile(dbPath, newPath, (err) => {
+            if(err) throw err;
+            console.log("Db copied to portable location")
+        })
+    }
+}
+
+//TODO move async dialog to background.js or make async call writing after finished
+export async function writeFile(fileName, data) {
+    const electron = require('electron').remote;
+    const app = electron.app;
+    const dialog = electron.dialog;
+    const mainWindow = electron.mainWindow;
+
+    const filePath = path.join(app.getPath("desktop"), fileName);
+    const result = await dialog.showSaveDialog(mainWindow, { 
+        defaultPath: filePath 
+    });
+
+    //How to writeFile multiple times 
+    const file = fs.createWriteStream(result.filePath);
+    file.write(data);
+    file.end();
+    file.close();
+    //TODO create callback for writeFile function
+    /*file.writeFile(result.filePath, data, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); */
 }
